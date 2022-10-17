a5b5cd01fc0d141eb2c7f9ba72359bfa5eb7bad9
JoaoBagvanji <joaobagvanji99@gmail.com>
2022-10-17 14:38:46 +0200

    update com JWT

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const fs = require('fs');
+const autenticacao = require('./middlewares/autenticacao')
 const app = express()
 const router = require('./routes/login_criar')
 
@@ -8,15 +8,12 @@ const router = require('./routes/login_criar')
 app.use(express.json())
 app.use(router)
 
-const users = [
-    { nome: 'Joao', idade: 23 },
-    { nome: 'Aline', idade: 22 },
-    { nome: 'Mateus', idade: 21 },
-    { nome: 'Liza', idade: 20 },
-]
-
-app.get("/", (req, res) => {
-    res.status(200).send('OK');
+app.get("/", autenticacao, (req, res) => {
+    res.status(200).send({
+        nome: req.user.nome,
+        email: req.user.email,
+        dados: req.user.dados,
+    });
 })
 
 app.listen(3000,  () =>{
--- routes/login_criar.js
+++ routes/login_criar.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
 const fs = require('fs')
+const jwt = require("jsonwebtoken")
 const router = express.Router()
 
 const dadosLocais = JSON.parse(fs.readFileSync('dados.json'))
@@ -10,15 +11,26 @@ router.post('/LogIn', (req, res) =>{
     const  { email, senha } = req.body
 
     if (!email || !senha){
-        res.status(422).send('Definir email e senha')
+        res.status(433).send('Definir email e senha')
     }
 
     const user = dadosLocais.find((usuario) =>usuario.email === email)
 
 
     if (!user) {
-        res.status(404).send('Usuário não existe')
+        res.status(401).send('Email ou senha inválidos')
     }
+
+    if (!bcrypt.compareSync(senha, user.hash)){
+        res.status(401).send('Senha inválida')
+    }
+
+    res.status(200).send({
+        email : user.email,
+        nome : user.nome, 
+        dados: user.dados,
+        token: user.token,
+    })
 
     
 })
@@ -31,16 +43,19 @@ router.post('/criar', (req, res) =>{
         res.status(401).send('Nome ou email ja em uso')
     } else {
         var dadosUsuario = {
+            id : Math.floor(Math.random() * 9999999999),
             nome : nome,
             email : email,
             dados : {},
         }
+        const token = jwt.sign({id: dadosUsuario.id}, "KEY_SECRETA")
+        dadosUsuario.token = token
         const salt = bcrypt.genSaltSync()
         dadosUsuario.hash = bcrypt.hashSync(senha, salt)
         dadosLocais.push(dadosUsuario)
         const dadosConvertidos = JSON.stringify(dadosLocais, null, 2)
         fs.writeFileSync('dados.json', dadosConvertidos)
-        res.status(200)
+        res.status(200).send("ok")
     }
 })
 
